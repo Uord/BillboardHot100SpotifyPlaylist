10c75c3d9543b0ff675f8f62be20af3bdd04b573
Uord <dzakrzewski878@gmail.com>
2022-11-06 22:23:52 +0100

    simple test

---
+++ test_billboard_scrapper.py
@@ -0,0 +1,6 @@
+from billboard_scrapper import scrape_chart
+
+data = [('Mariah Carey', 'All I Want For Christmas Is You'), ('Brenda Lee', "Rockin' Around The Christmas Tree"), ('Bobby Helms', 'Jingle Bell Rock'), ('Burl Ives', 'A Holly Jolly Christmas'), ('Adele', 'Easy On Me'), ('Andy Williams', "It's The Most Wonderful Time Of The Year"), ('Wham!', 'Last Christmas'), ('Jose Feliciano', 'Feliz Navidad'), ('The Kid LAROI & Justin Bieber', 'Stay'), ('The Ronettes', 'Sleigh Ride')]
+
+def test_scrape_chart():
+    assert scrape_chart("2022-01-01")[0:10] == data
