b07539753166b062534dd3a029e6bd9314f7cbf0
Uord <dzakrzewski878@gmail.com>
2022-11-07 18:18:23 +0100

    small tweaks in billboard_spotify_playlist

--- src/billboard_spotify_playlist.py
+++ src/billboard_spotify_playlist.py
@@ -25,6 +25,7 @@ def search_spotify(tracks: list[tuple[str,str]]) -> list[str]:
     """ Search Spotify for song URIs from songs and artists names
      and return a list of URIs """
     tracks_uri = []
+    
     for (artist,title) in tracks:
         result = sp.search(q=f"artist:{artist} track:{title}", type="track")
         try:
@@ -34,27 +35,28 @@ def search_spotify(tracks: list[tuple[str,str]]) -> list[str]:
             print(f"{artist}: {title} couldn't find in Spotify.")
     return tracks_uri
 
-# Create Spotify client
-sp = get_spotify_client()
+if __name__ == "__main__":
+    # Create Spotify client
+    sp = get_spotify_client()
 
-# Get Spotify user ID 
-user_id = sp.current_user()["id"]
+    # Get Spotify user ID 
+    user_id = sp.current_user()["id"]
 
-# Billboard Hot 100 date of searching
-date = input("Type the date (format YYYY-MM-DD) you want to search: ")
-try:
-    datetime.datetime.strptime(date, "%Y-%m-%d")
-except ValueError:
-    raise ValueError("Incorrect data format, should be YYYY-MM-DD")
+    # Billboard Hot 100 date of searching
+    date = input("Type the date (format YYYY-MM-DD) you want to search: ")
+    try:
+        datetime.datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Incorrect data format, should be YYYY-MM-DD")
 
-# Scrape Billboard 100 for list of (artist, song) tuples in a date
-tracks = scrape_chart(date)
+    # Scrape Billboard 100 for list of (artist, song) tuples in a date
+    tracks = scrape_chart(date)
 
-# Get songs URIs
-tracks_uri = search_spotify(tracks)
+    # Get songs URIs
+    tracks_uri = search_spotify(tracks)
 
-# Creating a playlist
-playlist = sp.user_playlist_create(user=user_id, name=f"Billboard 100 {date}", public=False)
+    # Creating a playlist
+    playlist = sp.user_playlist_create(user=user_id, name=f"Billboard 100 {date}", public=False)
 
-# Adding found tracks to the playlist
-sp.playlist_add_items(playlist_id=playlist["id"], items=tracks_uri)
+    # Adding found tracks to the playlist
+    sp.playlist_add_items(playlist_id=playlist["id"], items=tracks_uri)
